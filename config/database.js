4491be5dbb6a38c3d4c85f4cb1ac293742bc0342
Remy Truong <remtruong@Live.ca>
2019-04-24 15:25:32 -0700

    taking out some yucky stuff
    go get the relevant info yourself in the DB

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 var firebase = require('firebase');
 
 var config = {
-    apiKey: "AIzaSyAehd9h99xM3nR7rdsR_Vbab8-b4kgk76I",
+    apiKey: "",
     authDomain: "influx-trace.firebaseapp.com",
     databaseURL: "https://influx-trace.firebaseio.com",
     projectId: "influx-trace",
     storageBucket: "influx-trace.appspot.com",
-    messagingSenderId: "876821038783"
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
