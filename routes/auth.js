95f0c5e59387e624b701732507593da489bd06e1
suddenly-eva <suddenlyeva@gmail.com>
2019-04-30 14:29:34 -0700

    cleanup auth controller (async/await)

--- routes/auth.js
+++ routes/auth.js
@@ -5,45 +5,36 @@ var firebase = require('../config/database').firebase;
 var admin = require("../config/database").admin;
 
 if(!admin.apps.length){
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://influx-trace.firebaseio.com"
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://influx-trace.firebaseio.com"
+  });
 }
 
-router.post('/', function (req, res, next) {
-    if (!req.body.email) return res.status(400).json({ error: 'missing email' });
-    if (!req.body.password) return res.status(400).json({ error: 'missing password' });
+router.post('/', async (req, res, next) => {
 
-    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(async(resfb) => {
-        let idtoken = await resfb.user.getIdToken();
-        var adminstatus;
-        admin.auth().getUser(resfb.user.uid).then((record) => {
-            if (!record.customClaims) {
-                adminstatus = false;
-            } else {
-                adminstatus = record.customClaims.admin;
-            }
-        }).then(function () {
-            console.log(adminstatus);
-            res.send({ message: 'Logged in!', idToken: idtoken, admin: adminstatus }); 
-        })
-    }).catch(err => {
-      return res.status(401).json({error: err})
-    })
+    if (!req.body.email) {
+      return res.status(400).json({ error: 'missing email' });
+    }
+
+    if (!req.body.password) {
+      return res.status(400).json({ error: 'missing password' });
+    }
+
+    try {
+      let resfb   = await firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+      let idToken = await resfb.user.getIdToken();
+      let record  = await admin.auth().getUser(resfb.user.uid)
+      let admin   = false;
+      if (record.customClaims) {
+          admin = record.customClaims.admin;
+      }
+      res.send({ message: 'Logged in!', idToken, admin }); 
+    }
+    catch (error) {
+      res.status(401).json({error})
+    }
     
-});
-
-router.get('/', function (req, res, next) {
-
-    const maxResults = 100; 
-
-    admin.auth().listUsers(maxResults).then((userRecords) => {
-      userRecords.users.forEach((user) => console.log(user.toJSON()));
-      res.end('Retrieved users list successfully.');
-    }).catch((error) => console.log(error));
-
-
 });
 
 module.exports = router;
